b267bb7e22d3b0d2a0b2459a6ee1a837c7c1af6b
Timothy Lebo <timrdf@gmail.com>
2012-12-14 14:34:21 -0500

    situate classpaths relative to lib

--- src/vsr-situate-classpaths.sh
+++ src/vsr-situate-classpaths.sh
@@ -10,7 +10,7 @@ for jar in \
    $VSR_HOME/lib/csv2rdf4lod.jar
 do
    if [[ $CLASSPATH != *`basename $jar`* ]]; then
-      echo "`basename $jar` not in classpath; adding $CSV2RDF4LOD_HOME/$jar" >&2
+      echo "`basename $jar` not in classpath; adding $jar" >&2
       missing=$missing:$jar
    fi
 done
