ceab5cfa4beee4d38d31b4a892fe158b7a300913
Timothy Lebo <timrdf@gmail.com>
2012-12-14 14:39:52 -0500

    situate classpaths relative to lib

--- src/vsr-situate-classpaths.sh
+++ src/vsr-situate-classpaths.sh
@@ -4,6 +4,17 @@
 #3>    rdfs:seeAlso <https://github.com/timrdf/csv2rdf4lod-automation/wiki/situate-shell-paths-pattern> .
 
 VSR_HOME=$(cd ${0%/*} && echo ${PWD%/*})
+me=$(cd ${0%/*} && echo ${PWD})/`basename $0`
+
+if [ "$1" == "--help" ]; then
+   echo
+   echo "`basename $0` [--help]"
+   echo
+   echo "Put the required classpaths into CLASSPATH by executing:"
+   echo "    export CLASSPATH=\$CLASSPATH\`$me\`"
+   echo
+   exit
+fi
 
 missing=''
 for jar in \
