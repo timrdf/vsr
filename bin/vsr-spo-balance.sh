5e95f882166826b5ba4996b4956e665856032ec7
Tim L <timrdf@gmail.com>
2013-06-17 08:38:56 -0400

    moving to bin/ from src/

--- bin/vsr-spo-balance.sh
+++ bin/vsr-spo-balance.sh
@@ -6,6 +6,6 @@ VSR_HOME=$(cd ${0%/*} && echo ${PWD%/*})
 me=$(cd ${0%/*} && echo ${PWD})/`basename $0`
 
 export CLASSPATH=''
-export CLASSPATH=$CLASSPATH`$VSR_HOME/src/vsr-situate-classpaths.sh`
+export CLASSPATH=$CLASSPATH`$VSR_HOME/bin/vsr-situate-classpaths.sh`
 
 java edu.rpi.tw.visualization.overview.summarizer.RepositorySummarizer $*
